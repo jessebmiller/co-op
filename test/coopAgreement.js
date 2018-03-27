57c529a22bf0e33541c6651d89c06f51e4379720
Jesse B. Miller <jesse@jessebmiller.com>
2018-03-26 20:48:29 -0500

    more tests

--- test/coopAgreement.js
+++ test/coopAgreement.js
@@ -13,33 +13,49 @@ contract("CoopAgreements...", function ([alice, bob, ...accounts]) {
     bank.mint({from: bob, amount: 700})
   })
 
-  // agreement setup
-  it("Can be proposed, accepted and completed", async function() {
+  async function proposeAgreement(firstParty,
+                                  secondParty,
+                                  firstStake,
+                                  secondStake,
+                                  completionTimeLimit,
+                                 ) {
     const agreementText = "All who enter this agreement shall be satisfied."
     const agreementHash = web3.sha3(agreementText)
-    const aliceStake = 100
-    const bobStake = 70
-    const completionTimeLimit = 100 // time limit of 100 blocks
     const nonce = Math.random()
 
     // TODO write an agreement signing library function
     const proposal = {
       hash: agreementHash,
-      firstPartyStake: aliceStake,
-      secondPartyStake: bobStake,
+      firstParty: firstParty,
+      secondParty: secondParty,
+      firstPartyStake: firstStake,
+      secondPartyStake: secondStake,
       completionTimeLimit: completionTimeLimit,
       // nonce lets the proposal be uniquely identifiable by the first party
       // signature
       nonce: nonce
     }
-    const aliceSignature = web3.eth.sign(alice, JSON.stringify(proposal))
-    am.proposeAgreement(
-      agreementHash,
-      aliceStake,
-      bobStake,
-      completionTimeLimit,
-      aliceSignature,
+    const firstPartySignature = web3.eth.sign(
+      firstParty,
+      JSON.stringify(proposal),
     )
+    await am.proposeAgreement(
+      agreementHash,
+      secondParty,
+      firstStake,
+      secondStake,
+      completionTimeLimit,
+      firstPartySignature,
+      {from: firstParty},
+    )
+    return proposal
+  }
+  // agreement setup
+  it("Can be proposed, accepted and completed", async function() {
+
+    const aliceStake = 100
+    const bobStake = 70
+    const proposal = proposeAgreement(alice, bob, aliceStake, bobStake, 5555)
     const bobSignature = web3.eth.sign(bob, JSON.stringify(proposal))
     am.acceptProposal(proposal, bobStake, bobSignature)
 
@@ -63,14 +79,12 @@ contract("CoopAgreements...", function ([alice, bob, ...accounts]) {
     assert.equal(agreement.secondPartySatisfied, 0)
   })
 
-  it("Logs the agreement id and participants", async function() {
-    assert(false)
-  })
-
   it("Requires proposer has the balance to stake", async function() {
     bobBalance = await bank.balanceOf(bob)
+    aliceBalance = await bank.balanceOf(alice)
     try {
       // bob proposes an agreement with a stake more than they can afford
+      proposeAgreement(bob, alice, bobBalance + 1, aliceBalance, 5555)
     } catch(e) {
       assert.equal(
         "VM Exception while processing transaction: revert",
@@ -79,12 +93,43 @@ contract("CoopAgreements...", function ([alice, bob, ...accounts]) {
       return
     }
     assert.fail("Allowed over stake")
+  })
+
+  it("Requires balance to cover all proposals", async function() {
+    const bobBalance = await bank.balanceOf(bob)
+    const aliceBalance = await bank.balanceOf(alice)
+    const pa = await proposeAgreement(
+      bob,
+      alice,
+      bobBalance - 1,
+      aliceBalance,
+      5555,
+    )
+    try {
+      pb = await proposeAgreement(bob, accounts[0], 2, 2, 5555)
+    } catch(e) {
+      assert.equal(
+        "VM Exception while processing transaction: revert",
+        e.message,
+      )
+      return
+    }
+    assert.fail("allowed over propose")
   })
 
   it("Requires acceptor has the balance to stake", async function() {
-    bobBalance = await bank.balanceOf(bob)
+    const aliceBalance = await bank.balanceOf(alice)
+    const bobBalance = await bank.balanceOf(bob)
+    const proposal = await proposeAgreement(
+      alice,
+      bob,
+      aliceBalance,
+      bobBalance + 1,
+      5555,
+    )
     try {
-      // alice proposes an agreement that bob can't afford, bob tries to accept
+      const bobSignature = web3.eth.sign(bob, JSON.stringify(proposal))
+      am.acceptProposal(proposal, bobBalance + 1, bobSignature)
     } catch(e) {
       assert.equal(
         "VM Exception while processing transaction: revert",
@@ -92,17 +137,80 @@ contract("CoopAgreements...", function ([alice, bob, ...accounts]) {
       )
       return
     }
-    assert.fail("Allowed over stake")
+    assert.fail("Allowed accept over stake")
   })
 
   it("Requires proposers valid signature", async function() {
-    assert(false)
+    const proposal = {
+      hash: web3.sha3("Let's not sign it correctly."),
+      firstParty: alice,
+      secondParty: bob,
+      firstPartyStake: await bank.balanceOf(alice) / 2,
+      secondPartyStake: await bank.balanceOf(bob) / 2,
+      completionTimeLimit: 5555,
+      nonce: Math.random()
+    }
+    const invalidAliceSignature = web3.eth.sign(bob, JSON.stringify(proposal))
+    try {
+      await am.proposeAgreement(
+        proposal.hash,
+        proposal.secondParty,
+        proposal.firstPartyStake,
+        proposal.secondPartyStake,
+        proposal.completionTimeLimit,
+        proposal.nonce,
+        invalidAliceSignature,
+        {from: alice},
+      )
+    } catch(e) {
+      assert.equal(
+        "VM Exception while processing transaction: revert",
+        e.message,
+      )
+      return
+    }
+    assert.fail("accepted invalid signature")
+
   })
 
   it("Requires acceptors valid signature", async function() {
-    assert(false)
+    const proposal = {
+      hash: web3.sha3("Let's not sign it correctly."),
+      firstParty: alice,
+      secondParty: bob,
+      firstPartyStake: await bank.balanceOf(alice) / 2,
+      secondPartyStake: await bank.balanceOf(bob) / 2,
+      completionTimeLimit: 5555,
+      nonce: Math.random()
+    }
+    const AliceSignature = web3.eth.sign(alice, JSON.stringify(proposal))
+    await am.proposeAgreement(
+      proposal.hash,
+      proposal.secondParty,
+      proposal.firstPartyStake,
+      proposal.secondPartyStake,
+      proposal.completionTimeLimit,
+      proposal.nonce,
+      AliceSignature,
+      {from: alice},
+    )
+    const invalidBobSignature = web3.eth.sign(alice, JSON.stringify(proposal))
+    try {
+      am.acceptProposal(proposal, invalidBobSignature, JSON.stringify(proposal))
+    } catch(e) {
+      assert.equal(
+        "VM Exception while processing transaction: revert",
+        e.message,
+      )
+      return
+    }
+    assert.fail("accepted invalid signature")
   })
 
+  // discoverability: how can someone watch for proposals in which they are the
+  // second party and how can a first party inform a second party where to find
+  // the proposal
+ 
   // completion
   // test satisfaction matrix outcomes
 })
